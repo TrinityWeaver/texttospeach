77831e3d4b0fbc9e7c38d0c3da554ce7fdaefc11
Sebastian Marynicz <sebastian.marynicz@gmail.com>
2020-11-12 15:58:52 +0000

    Dodany Komentarz

--- texttospeach.py
+++ texttospeach.py
@@ -1,6 +1,6 @@
 from gtts import gTTS
 from playsound import playsound
-
+# Install appkit llb - pip3 install -U PyObjC
 
 name = input('Nadaj nazwe tekstu? ')
 tekst = input('Wpisz tekst, ktory program ma wypowiedziec: ')
