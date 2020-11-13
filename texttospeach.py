2533378df6b6729b78787b66870c72ac3839f5dc
Sebastian Marynicz <sebastianmarynicz@sebs-2019.home>
2020-11-13 22:32:02 +0000

    Office Work

--- texttospeach.py
+++ texttospeach.py
@@ -7,9 +7,6 @@ import os
 
 # Provide name for the file and text to speach
 
-# Install appkit llb - pip3 install -U PyObjC
-
-
 
 # Inputs from user
 
@@ -18,27 +15,17 @@ tekst = input('Wpisz tekst, ktory program ma wypowiedziec: ')
 
 
 # Modul gTTS, provide service Text to Speach
-# save file in mp3 format
 
+# Text to Speach conversion
+# Save file to mp3 format
 
-
-#Text to Speach conversion
-
-texttospeach = gTTS(text=tekst, lang='pl')
+texttospeach = gTTS(text=tekst, lang='en')
 texttospeach.save(name + '.mp3')
 
 
 # Modul playsound play sound
 
-# Save as a mp3
-
-
 playsound(name+'.mp3')
-
-
-
-# Adding to catalog mp3 ( Needs line for creating catalog)
-os.rename('/Users/sebastianmarynicz/Python/New_classes/OpenAG_course/Pieguska/'+ name+'.mp3','/Users/sebastianmarynicz/Python/New_classes/OpenAG_course/Pieguska/mp3/'+ name + '.mp.3')
 
 
 # if else statement Checks for folder "mp3", if exist it moves file there, if not it's creates folder and puts file in this folder
@@ -46,8 +33,14 @@ os.rename('/Users/sebastianmarynicz/Python/New_classes/OpenAG_course/Pieguska/'+
 if os.path.isdir(
         './mp3') == True:
     os.rename('./' + name+'.mp3',
-              './mp3/' + name + '.mp.3')
+              './mp3/' + name + '.mp3')
 else:
     os.mkdir('./mp3/')
     os.rename('./' + name+'.mp3',
-              './mp3/' + name + '.mp.3')
+              './mp3/' + name + '.mp3')
+
+
+# If file exists ask for overwrite
+# Ask user if he wants to save result
+# Choose Language
+# GUI
