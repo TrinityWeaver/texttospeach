fe7f095a12374cd8ba1798d68a4431ec794b7d9e
Sebastian Marynicz <sebastian.marynicz@gmail.com>
2020-11-13 10:37:04 +0000

    Comments Fixed

--- texttospeach.py
+++ texttospeach.py
@@ -1,14 +1,36 @@
+# Install appkit llb - pip3 install -U PyObjC
+
 from gtts import gTTS
 from playsound import playsound
-# Install appkit llb - pip3 install -U PyObjC
+import os
+
+
+# Provide name for the file and text to speach
 
 name = input('Nadaj nazwe tekstu? ')
 tekst = input('Wpisz tekst, ktory program ma wypowiedziec: ')
-#pelnyTekst = name + ' ' + tekst
 
+
+# Modul gTTS, provide service Text to Speach
+# Text to Speach conversion
+# Save file to mp3 format
 
 texttospeach = gTTS(text=tekst, lang='pl')
-
-
 texttospeach.save(name + '.mp3')
+
+
+# Modul playsound play sound
+
 playsound(name+'.mp3')
+
+
+# if else statement Checks for folder "mp3", if exist it moves file there, if not it's creates folder and puts file in this folder
+
+if os.path.isdir(
+        './mp3') == True:
+    os.rename('./' + name+'.mp3',
+              './mp3/' + name + '.mp.3')
+else:
+    os.mkdir('./mp3/')
+    os.rename('./' + name+'.mp3',
+              './mp3/' + name + '.mp.3')
