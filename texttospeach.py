dd144a0498f7741d97353e4ae2f1d1bab10301dc
Sebastian Marynicz <sebastianmarynicz@sebs-2019.home>
2020-11-13 00:01:27 +0000

    Comments_Folder_fix

--- texttospeach.py
+++ texttospeach.py
@@ -1,20 +1,32 @@
+# Install appkit llb - pip3 install -U PyObjC
+
 from gtts import gTTS
 from playsound import playsound
-import  os
+import os
 
-
-# Install appkit llb - pip3 install -U PyObjC
+# Provide name for the file and text to speach
 
 name = input('Nadaj nazwe tekstu? ')
 tekst = input('Wpisz tekst, ktory program ma wypowiedziec: ')
-#pelnyTekst = name + ' ' + tekst
+
+# Modul gTTS, provide service Text to Speach
+# save file in mp3 format
 
 
 texttospeach = gTTS(text=tekst, lang='pl')
-
-
 texttospeach.save(name + '.mp3')
+
+# Modul playsound play sound
+
 playsound(name+'.mp3')
-os.rename('/Users/sebastianmarynicz/Python/New_classes/OpenAG_course/Pieguska/'+ name+'.mp3','/Users/sebastianmarynicz/Python/New_classes/OpenAG_course/Pieguska/mp3/'+ name + '.mp.3')
 
+# if else statement Checks for folder "mp3", if exist it moves file there, if not it's creates folder and puts file in this folder
 
+if os.path.isdir(
+        './mp3') == True:
+    os.rename('./' + name+'.mp3',
+              './mp3/' + name + '.mp.3')
+else:
+    os.mkdir('./mp3/')
+    os.rename('./' + name+'.mp3',
+              './mp3/' + name + '.mp.3')
